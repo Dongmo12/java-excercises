c296ac4a20dc6a443009fed88f5569a55c9dc982
Dongmo12 <dongmoarmand@gmail.com>
2023-05-08 22:39:46 +0100

    Pushing my second project

---
+++ helloWolrdWithFlyway/helloWolrdWithFlyway/src/main/java/com/mobility/helloWolrdWithFlyway/HelloWolrdWithFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.mobility.helloWolrdWithFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWolrdWithFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWolrdWithFlywayApplication.class, args);
+	}
+
+}
---
+++ helloWolrdWithFlyway/helloWolrdWithFlyway/src/test/java/com/mobility/helloWolrdWithFlyway/HelloWolrdWithFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobility.helloWolrdWithFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWolrdWithFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
